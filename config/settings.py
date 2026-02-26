1d4378e5ef8ca1d2c535ea3272225010adc8ac5e
jupiterguy1234@gmail.com <jupiterguy1234@gmail.com>
2026-02-26 15:46:47 +0700

    Bug fixes

--- config/settings.py
+++ config/settings.py
@@ -149,8 +149,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # NEW: Where to collect static files for production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# NEW: WhiteNoise configuration for serving static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
